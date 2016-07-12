25e0a8d30295eed41db45cf799301e2dea2a02b9
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:56 +0200

    Update test to version 4.0

--- tests/test_stock_supply_production_warehouse.py
+++ tests/test_stock_supply_production_warehouse.py
@@ -3,23 +3,12 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'stock_supply_production_warehouse')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_supply_production_warehouse')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_supply_production_warehouse'
 
 
 def suite():
