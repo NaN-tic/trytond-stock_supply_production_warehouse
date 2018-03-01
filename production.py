4157ba0b49a2843980e55865e484e30e2cad8263
C?dric Krier <ced@b2ck.com>
2018-03-01 13:00:48 +0100

    Update to 4.7

--- production.py
+++ production.py
@@ -1,14 +1,14 @@
-#The COPYRIGHT file at the top level of this repository contains the full
-#copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
 
-__all__ = ['CreateProductionRequestStart', 'CreateProductionRequest']
-__metaclass__ = PoolMeta
+__all__ = ['StockSupplyStart', 'StockSupply']
 
 
-class CreateProductionRequestStart:
-    __name__ = 'production.create_request.start'
+class StockSupplyStart:
+    __metaclass__ = PoolMeta
+    __name__ = 'stock.supply.start'
 
     warehouses = fields.Many2Many('stock.location', None, None, 'Warehouses',
         domain=[
@@ -16,14 +16,13 @@ class CreateProductionRequestStart:
             ])
 
 
-class CreateProductionRequest:
-    __name__ = 'production.create_request'
+class StockSupply:
+    __metaclass__ = PoolMeta
+    __name__ = 'stock.supply'
 
     @property
-    def _requests_parameters(self):
-        res = super(CreateProductionRequest, self)._requests_parameters
+    def _production_parameters(self):
+        parameters = super(StockSupply, self)._requests_parameters
         if self.start.warehouses:
-            res.update({
-                    'warehouses': self.start.warehouses
-                    })
-        return res
+            parameters['warehouses'] = self.start.warehouses
+        return parameters
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from .test_stock_supply_production_warehouse import suite
+try:
+    from trytond.modules.stock_supply_production_warehouse.tests.test_stock_supply_production_warehouse import suite
+except ImportError:
+    from .test_stock_supply_production_warehouse import suite
+
+__all__ = ['suite']
